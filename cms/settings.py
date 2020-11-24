3f42dc7f0f68684adcbdc69965d5f6f9d8001f30
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-11-24 09:36:39 +0300

    Add heroku django package

--- cms/settings.py
+++ cms/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,4 +139,4 @@ LOGIN_REDIRECT_URL = 'dashboard'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-# import django_heroku
+django_heroku.settings(local())
